d6eb4b6b7db5af9b02d5a8daf23ac3117cfa87aa
Aleksey <kotbarsik96@apd.dev>
2026-03-26 18:00:01 +0400

    .

--- src/api/entities/Image/IImageEntity.ts
+++ src/api/entities/Image/IImageEntity.ts
@@ -2,13 +2,12 @@ import type { IImageEntityBase } from '@/api/entities/Image/IImageEntityBase'
 
 export interface IImageEntity extends IImageEntityBase {
   folder_id: number
-  s3_url: string
-  bucket: string
   key: string
   extension: string
   size_bytes: number
   width: number
   height: number
+  storage: string
   created_at: string
   updated_at: string
 }
